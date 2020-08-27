07d276b7397c14c7ee075697a85b74ecb5078f42
Emiliano Abarca <emiliano.abarca@extendeal.com>
2020-08-26 21:14:45 -0300

    fix conexión con mongo atlas

--- index.js
+++ index.js
@@ -33,7 +33,10 @@ console.log(require('dotenv').config());
 const app = require('./src/app');
 const mongoose = require('mongoose');
 // const connectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
-const connectionString = `${process.env.DB_HOST_DRIVER_CLOUD}://${process.env.DB_USER_CLOUD}:${process.env.DB_PASS_CLOUD}@${process.env.DB_HOST_CLOUD}/${process.env.DB_NAME_CLOUD}?retryWrites=true&w=majority`;
+//const connectionString = `${process.env.DB_HOST_DRIVER_CLOUD}://${process.env.DB_USER_CLOUD}:${process.env.DB_PASS_CLOUD}@${process.env.DB_HOST_CLOUD}/${process.env.DB_NAME_CLOUD}?retryWrites=true&w=majority`;
+
+const connectionString = "mongodb+srv://superuser:5QvN2qRnddWAMbxC@tpointeractivas.cchj1.gcp.mongodb.net/TPOInteracticas?retryWrites=true&w=majority&authSource=admin";
+
 
 if (process.env.NODE_ENV === 'development') {
   mongoose.set('debug', true);
