33f9d05cacdaf6bd9a99b35fa6a8920083ab64a5
Emiliano Abarca <emiliano.abarca@extendeal.com>
2020-08-27 21:12:40 -0300

    wip

--- src.maglio/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const users = require('./users');
-const games = require('./games');
-const played = require('./played');
-
-module.exports = {
-    users,
-    games,
-    played
-}
--- src.sarasa/routes/api.js
+++
@@ -1,8 +0,0 @@
-/**ROUTE USER APIs. */
-var express = require('express')
-
-var router = express.Router()
-var users = require('./api/user.route')
-router.use('/users', users);
-
-module.exports = router;
